835980e9fc0fab1bec64d98d67f86130ac07c8b9
Yves Junqueira <yves.junqueira@gmail.com>
2022-10-02 12:53:14 +0000

    all good appears to work

--- games.py
+++ games.py
@@ -40,97 +40,104 @@ if resp is None:
 
 # Make it look like a file
 pgnFile = StringIO(resp.json["pgn"]["pgn"])
-
-game = chess.pgn.read_game(pgnFile)
 i = 0
-continueFROM = 4
-while i < continueFROM:
-    game = chess.pgn.read_game(pgnFile)
+continueFROM = 0
+
+while True:
     i = i + 1
-    print("continuing analysis from", i)
+    if i < continueFROM:
+        continue
+    print("analyzing game", i)
+    game = chess.pgn.read_game(pgnFile)
 
-node = game
+    node = game
 
-target_player = "OopsKapootz"
-if game.headers["Black"] == target_player:
-    player_side = "B"
-else:
-    player_side = "W"
-
-cap = Cp(30)
-inaccuracy = -30
-mistake = -90
-blunder = -200
-
-print("Player side:", player_side)
-print(game.headers["Termination"])
-info = None
-
-while not node.is_end():
-    next_node = node.variations[0]
-    board = node.board()
-    move = board.san(next_node.move)
-    if board.turn:
-        side = "W"
+    target_player = "OopsKapootz"
+    if game.headers["Black"] == target_player:
+        player_side = "B"
     else:
-        side = "B"
+        player_side = "W"
+
+    cap = Cp(30)
+    inaccuracy = -30
+    mistake = -90
+    blunder = -200
+
+    print("\n\n===========================================")
+    pprint.pprint(game.headers)
+    print("Player side:", player_side)
+    print(game.headers["Termination"])
+    info = None
+
+    while not node.is_end():
+        next_node = node.variations[0]
+        board = node.board()
+        move = board.san(next_node.move)
+        if board.turn:
+            side = "W"
+        else:
+            side = "B"
 
 
-    capprior = cap
-    infoprior = info
-    # Do an engine evaluation of the position
-    # can either set depth or movetime - I am setting movetime for predictable analysis times
-    #bestmove, pondermove = engine.go(movetime = time)
-    info = engine.analyse(next_node.board(), chess.engine.Limit(depth=15))
+        capprior = cap
+        infoprior = info
+        # Do an engine evaluation of the position
+        # can either set depth or movetime - I am setting movetime for predictable analysis times
+        #bestmove, pondermove = engine.go(movetime = time)
+        info = engine.analyse(next_node.board(), chess.engine.Limit(depth=15))
 
-    import pdb
-    cap=info["score"].white()
-    pprint.pprint("white score {}".format(cap))
-    mate = info["score"].is_mate()
-    depth = info["depth"]
-    # by convention in this code, cpdelta of less than -200 bad, no matter what color
-    cpdelta = cap.score(mate_score=10000)-capprior.score(mate_score=10000)
-    # We want this to show as a negative cpdelta for black (meaning a blunder)
-    #20 W move O-O cap +218 delta -191
-    #21 B move Qe7 cap +420 delta 202
-    if side == "B":
-        cpdelta = -cpdelta
-    ply = board.ply()
-    print(ply, side, "move", move, "cap", cap, "delta", cpdelta)
-    print("prev cap", capprior.score(mate_score=10000), "curr cap", cap.score(mate_score=10000))
-    if ply == 39:
-        pdb.set_trace()
-    # pv is not in info when a mate was played, for example.
-    if side == player_side and "pv" in info:
-        print("cpdelta", cpdelta)
-        #if cpdelta < blunder:
-        if cpdelta < blunder:
-            #blunderinfo = engine.analyse(next_node.board(), chess.engine.Limit(depth=15))
-            bestreply = info["pv"][0]
-            bestmove = infoprior["pv"][0]
-            # TODO: This is detecting positive CP changes as well
-            print("Blunderino game {} ply {} ! cpdelta {}".format(i, ply, cpdelta))
-            print("move was", move)
-            print("move should have been", bestmove)
-             # show the board before the blunder
-            svg = chess.svg.board(board, flipped=(player_side == "B"))
-            out = open("{:0>5d}-game-{:0>4d}-ply-1-front-of-card.svg".format(i, ply), "w")
-            out.write(svg)
-            out.close()
-            # show the blunder
-            svg = chess.svg.board(next_node.board(), lastmove=next_node.move,
-                    flipped=(player_side == "B"), colors={'square dark lastmove':'red',
-                        'square light lastmove':'red'})
-            out = open("{:0>5d}-game-{:0>4d}-ply-2-hint.svg".format(i, ply), "w")
-            out.write(svg)
-            out.close()
-            # show the blunder and the solution
-            svg = chess.svg.board(next_node.board(), lastmove=next_node.move,
-                    flipped=(player_side == "B"), colors={'square dark lastmove':'red',
-                        'square light lastmove':'red'}, arrows=[[bestmove.from_square, bestmove.to_square],
-                            chess.svg.Arrow(bestreply.from_square, bestreply.to_square, color="red")])
-            out = open("{:0>5d}-game-{:0>4d}-ply-3-back-of-card.svg".format(i, ply), "w")
-            out.write(svg)
-            out.close()
-    board.push(next_node.move)
-    node = next_node
+        import pdb
+        cap=info["score"].white()
+        pprint.pprint("white score {}".format(cap))
+        mate = info["score"].is_mate()
+        depth = info["depth"]
+        # by convention in this code, cpdelta of less than -200 bad, no matter what color
+        cpdelta = cap.score(mate_score=10000)-capprior.score(mate_score=10000)
+        # We want this to show as a negative cpdelta for black (meaning a blunder)
+        #20 W move O-O cap +218 delta -191
+        #21 B move Qe7 cap +420 delta 202
+        if side == "B":
+            cpdelta = -cpdelta
+        ply = board.ply()
+        print(ply, side, "move", move, "cap", cap, "delta", cpdelta)
+        print("prev cap", capprior.score(mate_score=10000), "curr cap", cap.score(mate_score=10000))
+        #if ply == 39:
+        #    pdb.set_trace()
+        # pv is not in info when a mate was played, for example.
+        if side == player_side and "pv" in info:
+            print("cpdelta", cpdelta)
+            #if cpdelta < blunder:
+            if cpdelta < blunder:
+                #blunderinfo = engine.analyse(next_node.board(), chess.engine.Limit(depth=15))
+                bestreply = info["pv"][0]
+                bestmove = infoprior["pv"][0]
+                # TODO: This is detecting positive CP changes as well
+                print("Blunderino game {} ply {} ! cpdelta {}".format(i, ply, cpdelta))
+                print("move was", move)
+                print("move should have been", bestmove)
+                 # show the board before the blunder
+                svg = chess.svg.board(board, flipped=(player_side == "B"))
+                out = open("{:0>5d}-game-{:0>4d}-ply-1-front-of-card.svg".format(i, ply), "w")
+                out.write(svg)
+                out.close()
+                # show the blunder
+                svg = chess.svg.board(next_node.board(), lastmove=next_node.move,
+                        flipped=(player_side == "B"), colors={'square dark lastmove':'red',
+                            'square light lastmove':'red'})
+                out = open("{:0>5d}-game-{:0>4d}-ply-2-hint.svg".format(i, ply), "w")
+                out.write(svg)
+                out.close()
+                # show the blunder and the solution
+                svg = chess.svg.board(next_node.board(), lastmove=next_node.move,
+                        flipped=(player_side == "B"), colors={'square dark lastmove':'red',
+                            'square light lastmove':'red'}, arrows=[[bestmove.from_square, bestmove.to_square],
+                                chess.svg.Arrow(bestreply.from_square, bestreply.to_square, color="red")])
+                out = open("{:0>5d}-game-{:0>4d}-ply-3-back-of-card.svg".format(i, ply), "w")
+                out.write(svg)
+                out.close()
+        board.push(next_node.move)
+        node = next_node
+
+    game = chess.pgn.read_game(pgnFile)
+    if game is None:
+        os.exit(1)
